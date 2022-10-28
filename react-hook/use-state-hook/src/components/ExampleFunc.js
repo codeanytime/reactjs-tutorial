53097bfbff462ed999da73ee0fa47496b17aac6f
sonnth <sonnth@nal.vn>
2022-10-28 22:37:58 +0700

    useEffect tutorial

--- react-hook/use-state-hook/src/components/ExampleFunc.js
+++ react-hook/use-state-hook/src/components/ExampleFunc.js
@@ -1,24 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 
-const init = () => {
-    let total = 0;
-    for (let i = 0; i < 100; i++) {
-        total += i;
-    }
-    return total;
-}
+// neu init value cua state la ham thi dung lamda function de tranh re render ham nhieu lan
 
 function ExampleFunc() {
-    const [count, setCount] = useState(() => init());
+    const [count, setCount] = useState(0);
+    const [action, setAction] = useState('');
+    const [scrollPosition, setScrollPosition] = useState(0);
+    
     const handleClick = () => {
         setCount(count + 1);
     }
 
+    useEffect(() => {
+        document.title = `you click ${count} times`;
+        console.log('Effect')
+
+        return () => {
+            console.log("Clean up function");
+        }
+    }, [count])
+
+    useEffect(() => {
+        fetch(`https://reqres.in/api/${action}`)
+        .then(res => console.log({res}))
+        .catch(err => console.log({err}));
+    }, [action])
+
+    const handleScroll = () => {
+        setScrollPosition(window.scrollY);
+    }
+    useEffect(() => {
+        //componentDidMount
+        document.addEventListener('scroll', handleScroll)
+        return () => {
+            // componentWillUnmount
+            document.removeEventListener('scroll', handleScroll)
+        }
+    })
+
     return (
-        <div>
+        <div style={{height: '3000px'}}>
             <h1>Function component</h1>
             <h2>You click {count} times</h2>
             <button onClick={handleClick}>Click me</button>
+            <button onClick={() => setAction('users')}>Get user</button>
+            <button onClick={() => setAction('comments')}>Get comments</button>
+            <p style={{position: 'fixed', bottom: '20px', left: '20px'}}>{scrollPosition}</p>
         </div>
     )
 }
